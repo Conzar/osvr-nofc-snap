89ea52df3c1d0549558bb9f638aeea7a7ddc2252
DevOps <devops@monky-games.com>
2017-11-22 11:32:37 +1300

    Working on snap definitions.

--- prepare.bash
+++ prepare.bash
@@ -2,3 +2,6 @@
 git clone https://bitbucket.org/monkygames/osvr-core-ubuntu-build-script build-script
 $(pwd)/build-script/build.bash -d /opt/build -p -s -n -r -f -i $(pwd)/build-script/bash_lib
 
+# delete osvr_config file
+#rm $(pwd)/dist/osvr_server_config.json
+
